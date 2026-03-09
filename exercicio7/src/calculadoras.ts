28b7da7671b8f730cbf634cc8d419f6960a3b447
KauanHSP <kauanprado279@gmail.com>
2026-03-09 18:57:54 -0400

    exer7 +

---
+++ exercicio7/src/calculadoras.ts
@@ -0,0 +1,21 @@
+export default class calculadora {
+    num1: number;
+    num2: number;
+
+    constructor(num1:number,num2:number){
+        this.num1 = num1
+        this.num2 =num2
+    }
+    somar():void{
+        console.log(this.num1 + this.num2)        
+    }
+    subtrair():void{
+        console.log(this.num1 - this.num2)
+    }
+    multiplicar():void{
+        console.log(this.num1 * this.num2)
+    }
+    dividir():void{
+        console.log(this.num1 / this.num2)
+    }
+}
---
+++ exercicio7/src/index.ts
@@ -0,0 +1,8 @@
+import calculadora from "./calculadoras.ts";
+
+const cal1 = new calculadora(10,5)
+
+cal1.somar()
+cal1.subtrair()
+cal1.multiplicar()
+cal1.dividir()
