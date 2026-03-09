95cce3c89b54ac3cd1d470bc394d478558a95fac
KauanHSP <kauanprado279@gmail.com>
2026-03-09 16:51:17 -0400

    firt commit

---
+++ exercicio1/src/index.ts
@@ -0,0 +1,5 @@
+import Pessoa from "./exrc1.ts";
+
+const cidadao = new Pessoa("maria", 30)
+
+console.log(cidadao)
---
+++ exercicio2/src/Cachorros.ts
@@ -0,0 +1,9 @@
+export default class cachorro {
+    nome: string;
+    raca: string;
+
+    constructor(nome:string, raca:string){
+        this.nome
+        this.raca     
+    }
+}
